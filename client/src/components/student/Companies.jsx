d1ca7f44b22929b1b08c6787bbc3cc896e909344
mohangrg2018 <mohangrg2030@gmail.com>
2025-05-16 11:59:22 +0545

    companies section added

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -1,7 +1,30 @@
+import { assets } from "@/assets/assets";
 import React from "react";
 
 const Companies = () => {
-  return <div>Companies</div>;
+  return (
+    <section>
+      <p className="text-base text-gray-500">Trusted by learners from</p>
+      <div className="flex flex-wrap items-center justify-center gap-6 md:gap-16 mt-8">
+        <img
+          src={assets.microsoft_logo}
+          alt="microsoft"
+          className="w-20 md:w-28"
+        />
+        <img
+          src={assets.walmart_logo}
+          alt="microsoft"
+          className="w-20 md:w-28"
+        />
+        <img src={assets.adobe_logo} alt="microsoft" className="w-20 md:w-28" />
+        <img
+          src={assets.accenture_logo}
+          alt="microsoft"
+          className="w-20 md:w-28"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Companies;
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,3 +1,4 @@
+import Companies from "@/components/student/Companies";
 import Hero from "@/components/student/Hero";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <main className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies />
     </main>
   );
 };
