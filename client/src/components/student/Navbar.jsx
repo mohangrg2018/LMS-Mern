5a795875a08ba0f5876a40f1cae7dff868803839
mohangrg2018 <mohangrg2030@gmail.com>
2025-06-01 13:52:23 +0545

    clerk installed and setup done

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,7 +1,58 @@
+import { assets } from "@/assets/assets";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { AppContext } from "@/context/AppContext";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // const { navigate } = useContext(AppContext);
+
+  const { openSignUp } = useClerk();
+  const { user } = useUser();
+
+  const isCourseListPage = location.pathname.includes("course-list");
+  return (
+    <header
+      className={`py-4 sticky top-0 border-b ${
+        isCourseListPage ? "bg-white" : "bg-cyan-200/30"
+      }`}
+    >
+      <div className="container__width flex justify-between items-center">
+        <Link to="/">
+          <img src={assets.logo} alt="site logo" />
+        </Link>
+        <div className="hidden md:flex items-center gap-6">
+          {user && (
+            <>
+              <Link to="/educator">Become Educator</Link>|
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
+          {user ? (
+            <UserButton />
+          ) : (
+            <Button onClick={() => openSignUp()}>Create Account</Button>
+          )}
+        </div>
+
+        {/* For Smaller Screens */}
+        <div className="md:hidden flex items-center gap-6">
+          {user && (
+            <>
+              <Link to="/educator">Become Educator</Link>|
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
+          {user ? (
+            <UserButton />
+          ) : (
+            <Button onClick={() => openSignUp()}>Create Account</Button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,7 +1,9 @@
 import { AppContext } from "./AppContext";
+import { useNavigate } from "react-router-dom";
 
 const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const value = { navigate };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
